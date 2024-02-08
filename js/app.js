42ac50c6f4ffa4b0729b370505904a6987b04cbd
VoidTheStars <158555799+VoidTheStars@users.noreply.github.com>
2024-02-07 20:30:44 -1000

    Patch 7
    +Modified the third button on user request.
    +Added a gold counter.
    +Filling the progress bar now awards one gold.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
+money = 0;
+
 function jottem()  {
   let temp = document.getElementById("status");
   if (temp.value === 100) {
     temp.value = 0;
+    money+= 1;
+    refresh();
   } else {
     temp.value += 10;
   }
+}
+
+function refresh(){
+  document.getElementById("gold").innerHTML = "Gold : " + money;
 }
